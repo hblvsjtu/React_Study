112b2bdb33a495e38d853ce531cdbac52b5f42be
hblvsjtu <hblvsjtu@163.com>
2018-05-24 11:37:29 +0800

    2018-05-24
    1，Babel的环境安装

---
+++ EnvironmentCreat/compiled.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var a = [1, 2, 3];
+var b = a.map(function (ele) {
+  return ele * 2;
+});
+console.log(b);
---
+++ EnvironmentCreat/jsTest.js
@@ -0,0 +1,3 @@
+var a = [1, 2, 3];
+var b = a.map((ele) => ele*2) ;
+console.log(b);
